d675287727679844ffe8d15f2de68e9f36e8ff19
Mohit Sudhirbhai Vora <Mohit_Vora@ad.infosys.com>
2017-08-10 12:46:05 +0530

    latest mohit all same page

--- src/PageObjects/Transactions.java
+++ src/PageObjects/Transactions.java
@@ -20,20 +20,6 @@ public class Transactions {
 	}
 
 
-=======
-public class Transactions {
-
-	public void PopulatePaymenttoMember(WebDriver driver) {
-		String Logged_user = driver.findElement(rd1.getLocator("ELM_LOGGEDUSER")).getText();
-		System.out.println(Logged_user);
-		String[] User = Logged_user.split(" ", 5);
-		if (User[2].equalsIgnoreCase("admin")) {
-
-		} else {
-
-		}
-
-	}
 
 
 }
