6d0ee8befef2ee03b4cfb68b9d30dee467f51d11
Mohit Sudhirbhai Vora <Mohit_Vora@ad.infosys.com>
2017-08-10 11:11:59 +0530

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 61523c4800c9ddc0c5faca514dab312a666d382c, reversing
    changes made to f9e911747152ee653308d1b4e8c7aaba5eb2cc73.

---
+++ src/PageObjects/VerifyPopUp.java
@@ -0,0 +1,23 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class VerifyPopUp {
+	
+	public void PopUpAccept(WebDriver driver){
+		try
+		{
+			String PopUpMessage = driver.switchTo().alert().getText();
+			driver.switchTo().alert().accept();
+		}
+			catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		//System.out.println(PopUpMessage);
+		//return PopUpMessage;
+
+		
+	}
+
+}
---
+++ src/Utils/BrowserUtils.java
@@ -0,0 +1,57 @@
+package Utils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class BrowserUtils {
+	public static WebDriver driver = null;
+	
+
+	String Url = PropRead.getVal("url");
+
+	/*public WebDriver driver(){
+		return driver;
+	}*/
+	
+
+  
+  public WebDriver openbrowserie() {
+	    System.setProperty("webdriver.ie.driver",
+				"D:\\Reference Selenium\\Jars and Drivers\\IEDriverServer_x64_2.53.1\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+	
+		driver.get(Url);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+  }
+  
+  public WebDriver openbrowserChrome() {
+	    System.setProperty("webdriver.chrome.driver",
+	    		PropRead.getVal("chromeDriver"));
+		driver = new ChromeDriver();
+		driver.get(Url);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+}
+  
+  
+ public void Closebrowser() throws InterruptedException{
+	 Thread.sleep(3000);
+		driver.quit();
+		
+		try {
+		    Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+
+ }
+	
+}
+
