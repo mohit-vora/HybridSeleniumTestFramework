bf21bd4e2223576319a81e5bac2050cad7930e55
Mohit Sudhirbhai Vora <Mohit_Vora@ad.infosys.com>
2017-08-10 11:06:53 +0530

    Revert "integrated testng"
    This reverts commit f9e911747152ee653308d1b4e8c7aaba5eb2cc73.

--- src/Driver/OneClickRun.java
+++ src/Driver/OneClickRun.java
@@ -1,26 +1,10 @@
 package Driver;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.testng.TestNG;
-
 public class OneClickRun {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		
-		
-		runTestNG();
-	}
-	
-	public static void runTestNG()
-	{
-		TestNG runner=new TestNG();
-		List<String> suitefiles=new ArrayList<String>();
-		suitefiles.add(System.getProperty("user.dir")+"\\testng.xml");
-		runner.setTestSuites(suitefiles);
-		runner.run();
 	}
 
 }
